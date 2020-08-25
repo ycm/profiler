6859726938fd15a04ba88dc2297c5057ed05d5c4
ycm <ycm@stanford.edu>
2020-08-25 01:24:09 -0700

    more updates to identify-difficulties

--- identify-difficulties/identify_difficulties_util.py
+++ identify-difficulties/identify_difficulties_util.py
@@ -2,7 +2,12 @@
 
 # identify_difficulties_util.py
 
+import math
 import pandas as pd
+
+# def truncate(number, digits):
+#     stepper = 10.0 ** digits
+#     return math.trunc(stepper * number) / stepper
 
 def find_problem_words_given_df_naive(perf_df, pause_threshold, expected_pauses):
     token_list            = perf_df.token
@@ -23,16 +28,19 @@ def find_problem_words_given_df_naive(perf_df, pause_threshold, expected_pauses)
     return rv
 
 def helper_find_overlap_proportion(D, Q):
+    if not Q:
+        return -1
     return len(set(D) & set(Q)) / len(Q)
 
 
 def compute_difficult_words_overlap_proportion(
     difficult_words_indices,
+    column_name,
     text_analysis_column_1,
     text_analysis_column_2=None,
     text_analysis_column_3=None,
     mode='binary',
-    n_bins_if_continuous=5):
+    n_splits_if_continuous=5):
     assert mode in {'binary', 'discrete', 'continuous'}, 'Invalid mode.'
 
     # difficult_words_indices are indices of a PERF MATRIX.
@@ -58,8 +66,8 @@ def compute_difficult_words_overlap_proportion(
         )
 
         return {
-            'true_overlap_proportion': true_overlap_proportion,
-            'false_overlap_proportion': false_overlap_proportion
+            column_name + '_true_overlap_proportion': true_overlap_proportion,
+            column_name + '_false_overlap_proportion': false_overlap_proportion
         }
 
     assert text_analysis_column_2 and text_analysis_column_3,\
@@ -89,15 +97,53 @@ def compute_difficult_words_overlap_proportion(
                 difficult_indices, more_than_value_indices
             )
 
-            rv['less_than_' + str(separating_value) + '_proportion'] =\
-                less_than_value_overlap_proportion
-            rv['more_than_' + str(separating_value) + '_proportion'] =\
-                more_than_value_overlap_proportion
+            if less_than_value_overlap_proportion >= 0 and\
+                more_than_value_overlap_proportion >= 0:
+                rv[column_name + '_less_than_' + str(separating_value) +\
+                    '_proportion'] = less_than_value_overlap_proportion
+                rv[column_name + '_more_than_' + str(separating_value) +\
+                    '_proportion'] = more_than_value_overlap_proportion
 
         return rv
 
     if mode == 'continuous':
-        pass
+        all_values = text_analysis_column_1 + text_analysis_column_2 +\
+            text_analysis_column_3
+        min_value, max_value = min(all_values), max(all_values)
+
+        bin_size = (max_value - min_value) / (n_splits_if_continuous + 1)
+
+        rv = {}
+        for i in range(n_splits_if_continuous):
+            separating_value = min_value + (i + 1) * bin_size
+            less_than_value_indices = [
+                idx for idx, value in enumerate(text_analysis_column_1)
+                if value < separating_value
+            ]
+
+            less_than_value_overlap_proportion = helper_find_overlap_proportion(
+                difficult_indices, less_than_value_indices
+            )
+
+            more_than_value_indices = [
+                idx for idx, value in enumerate(text_analysis_column_1)
+                if value >= separating_value
+            ]
+
+            more_than_value_overlap_proportion = helper_find_overlap_proportion(
+                difficult_indices, more_than_value_indices
+            )
+
+            if less_than_value_overlap_proportion >= 0 and\
+                more_than_value_overlap_proportion >= 0:
+                rv[column_name + '_less_than_' + str(separating_value) +\
+                    '_proportion'] = less_than_value_overlap_proportion
+                rv[column_name + '_more_than_' + str(separating_value) +\
+                    '_proportion'] = more_than_value_overlap_proportion
+
+        return rv
+
+        
 
 
 
